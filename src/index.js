40d1edbf0ce8b11eed0c541d4e0484cbb7da4f8d
Filip Veličković <filipvkovic@gmail.com>
2017-11-13 09:10:58 +0100

    add multi states prop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { Component } from "react"
+import { Component } from "react";
 
 const eventsChanged = (yeoldevents, yonnewevents) =>
-  yeoldevents.sort().toString() !== yonnewevents.sort().toString()
+  yeoldevents.sort().toString() !== yonnewevents.sort().toString();
 
 export default class Idle extends Component {
   static defaultProps = {
@@ -10,62 +10,80 @@ export default class Idle extends Component {
     onChange: () => {},
     timeout: 1000,
     events: ["mousemove", "mousedown", "keydown", "touchstart", "scroll"]
-  }
+  };
 
   state = {
     idle: this.props.defaultIdle
-  }
+  };
 
-  timeout = null
+  timeout = null;
 
   componentDidMount() {
-    this.attachEvents()
-    this.setTimeout()
+    this.attachEvents();
+    this.setTimeout();
   }
 
   componentWillUnmount() {
-    this.removeEvents()
+    this.removeEvents();
   }
 
   componentDidUpdate(prevProps) {
     if (eventsChanged(prevProps.events, this.props.events)) {
-      this.removeEvents()
-      this.attachEvents()
+      this.removeEvents();
+      this.attachEvents();
     }
   }
 
   attachEvents() {
     this.props.events.forEach(event => {
-      window.addEventListener(event, this.handleEvent, true)
-    })
+      window.addEventListener(event, this.handleEvent, true);
+    });
   }
 
   removeEvents() {
     this.props.events.forEach(event => {
-      window.removeEventListener(event, this.handleEvent)
-    })
+      window.removeEventListener(event, this.handleEvent);
+    });
   }
 
   handleChange(idle) {
-    this.props.onChange({ idle })
-    this.setState({ idle })
+    this.props.onChange({ idle });
+    this.setState({ idle });
   }
 
   handleEvent = () => {
     if (this.state.idle) {
-      this.handleChange(false)
+      this.handleChange(false);
     }
-    clearTimeout(this.timeout)
-    this.setTimeout()
-  }
+    if (!this.props.states) {
+      clearTimeout(this.timeout);
+    } else {
+      for (let i = 0; i < this.stateTimers.length; i++) {
+        clearTimeout(this.stateTimers[i]);
+      }
+    }
+    this.setTimeout();
+  };
 
   setTimeout() {
-    this.timeout = setTimeout(() => {
-      this.handleChange(true)
-    }, this.props.timeout)
+    if (!this.props.states) {
+      this.timeout = setTimeout(() => {
+        this.handleChange(true);
+      }, this.props.timeout);
+    } else {
+      this.stateTimers = [];
+      for (let i = 0; i < this.props.states.length; i++) {
+        this.stateTimers.push(
+          setTimeout(() => {
+            debugger;
+            this.handleChange(this.props.states[i].id);
+          }, this.props.states[i].timeout)
+        );
+      }
+    }
   }
 
   render() {
-    return this.props.render(this.state)
+    return this.props.render(this.state);
   }
 }
